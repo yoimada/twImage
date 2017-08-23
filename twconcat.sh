a60118c1b22d1c1bcbcb3bb89e2c35bb2ea520e9
yoimada <rudenenet@yahoo.co.jp>
2017-08-23 23:21:17 +0900

    Update twconcat.sh

--- twconcat.sh
+++ twconcat.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-cat "$@" | sed -e s/\<br\>/\<BR\>/g -e s/\"//g -e s/\'//g -e s/^M/g| sort -ru
+#!/bin/bash
+cat "$@" | sed -e s/\<br\>/\<BR\>/g -e s/\"//g -e s/\'//g -e s/^M//g | sort -ru | grep -v -f <( grep -o -h -e "[0-9a-zA-Z\_\-]\+.\(jpg\|png\|gif\|jpeg\):orig" done/* | sort -u )
 ## Control Charactor ^M
+
+## done/* => looked html
+## args => twImage.js generated html file"s"
